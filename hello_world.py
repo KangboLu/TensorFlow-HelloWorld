cf7ba9f2d59d57a57900eefe85cab72af5eb96fe
KangboLu <redstonegranola@gmail.com>
2018-07-26 13:07:51 +0800

    train with gradient descend optimizer

--- hello_world.py
+++ hello_world.py
@@ -27,3 +27,10 @@ y = tf.nn.softmax(tf.matmul(x, W) + b)
 # define loss function with cross entropy input with label
 y_ = tf.placeholder(tf.float32, [None, 10])
 cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+
+# train with gradient descent optimizer
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+tf.global_variables_initializer().run()
+for i in range(1000):
+	batch_xs, batch_ys = mnist.train.next_batch(100)
+	train_step.run({x: batch_xs, y_: batch_ys})
