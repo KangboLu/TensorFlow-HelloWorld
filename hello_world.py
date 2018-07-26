e4a9d382ac21e1247fa2454b1944606c2f0411ce
KangboLu <redstonegranola@gmail.com>
2018-07-26 13:05:58 +0800

    peek input data dimension

--- hello_world.py
+++ hello_world.py
@@ -2,3 +2,12 @@
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 print("- MNIST dataset imported\n")
+
+# output the format of train, test, and validation data
+print("- Trainning data and label dimension:")
+print(mnist.train.images.shape, mnist.train.labels.shape)
+print("- Testing data and label dimension:")
+print(mnist.test.images.shape, mnist.test.labels.shape)
+print("- Validation data and label dimension:")
+print(mnist.validation.images.shape, mnist.validation.labels.shape)
+print
